f8cb19781247039a8f45c2c8f4beaf1d7c6ebabe
felps <luisfelipeamorim@hotmail.com>
2024-10-14 21:33:15 -0300

    chore: remoção de helpers

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,12 @@ import { Hono } from "hono";
 import api from "./routes";
 import { logger } from "hono/logger";
 import { cors } from "hono/cors";
-import { prettyJSON } from "hono/pretty-json";
 import { customLogger } from "./middlewares/logger.middleware";
-import { requestId } from "hono/request-id";
 import "./lib/instrument";
 
 const app = new Hono();
 
-app.use(prettyJSON());
 app.use(cors());
-app.use(requestId());
 app.use(logger(customLogger));
 
 app.route("/api", api);
