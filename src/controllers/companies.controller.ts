03111a2c9d6ac76c14d165b8035e4b44f36e97f6
Vitor Machado <usl.contatos@gmail.com>
2024-10-16 23:46:02 -0300

    fix: ajuste

--- src/controllers/companies.controller.ts
+++ src/controllers/companies.controller.ts
@@ -1,34 +1,43 @@
 import { prisma } from "../lib/db";
 
 export async function getByUserId(userId: number) {
+  console.log(userId);
+  const result = await prisma.company.findMany({
+    where: {
+      Users: {
+        some: {
+          id: userId,
+        },
+      },
+    },
+  });
 
-	console.log(userId)
-	const result = await prisma.company.findMany({
-		where: {
-			Users: {
-				some: {
-					id: userId
-				}
-			}
-		}
-	})
+  console.log(result);
 
-	console.log(result)
-
-	return result
+  return result;
 }
-
 
 export async function getByEmployeeId(employeeId: number) {
+  const enrolments = await prisma.employeeEnrolment.findMany({
+    where: {
+      employeeId,
+    },
+    select: {
+      company: {
+        select: {
+          name: true,
+          id: true,
+        },
+      },
+    },
+  });
 
-	const enrolment = await prisma.employeeEnrolment.findMany({
-		where: {
-			employeeId
-		}
-	})
+  const companies = enrolments.map((enrolment) => ({
+    name: enrolment.company.name,
+    id: enrolment.company.id,
+  }));
 
-	console.log(enrolment)
+  console.log(companies);
 
-	return enrolment
+  return companies;
 }
-
