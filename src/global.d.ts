c7920ddce80a8d06c701110381a9a0a89d1303e1
Luis Amorim <luisfelipeamorim@hotmail.com>
2025-12-29 16:31:34 -0300

    Filesystem (#13)
    * feat(recibo): Tratativa de matricula com 1 digito
    
    * feat(recibos): Tigris
    - MIgração para o tigris

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,23 +3,23 @@ declare namespace NodeJS {
 		NODE_ENV: "development" | "production" | "test";
 		TURSO_DATABASE_URL: string;
 		TURSO_AUTH_TOKEN: string;
-		
+
 		EMAIL_USER: string;
 		EMAIL_PASSWORD: string;
 		EMAIL_SMTP: string;
 		EMAIL_PORT: string;
-		
+
 		JWT_SECRET: string;
-		
+
 		REFRESH_TOKEN_SECRET: string;
 		ACCESS_TOKEN_SECRET: string;
-		
+
 		S3_ACCESS_KEY_ID: string;
-		S3_SECRET_KEY: string;
+		S3_SECRET_ACCESS_KEY: string;
 		S3_URL: string;
 		S3_BUCKET_NAME: string;
 		S3_BUCKET_DOMAIN: string;
-		
+
 		PORT: string;
 		NODE_ENV: string;
 	}
